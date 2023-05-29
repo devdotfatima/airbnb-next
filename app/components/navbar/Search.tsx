099f6d359a96ad05e88ffef98245b11efe0154b1
Fatima Ali <fatimaaliforwork@gmail.com>
2023-05-29 08:08:42 +0500

    creates NavBar UI

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { BiSearch } from "react-icons/bi";
+const Search = () => {
+  return (
+    <div
+      className="
+                    border-[1px]
+                    w-full
+                    md:w-auto
+                    py-2
+                    rounded-full
+                    shadow-sm
+                    hover:shadow-md
+                    transition
+                    cursor-pointer
+
+  "
+    >
+      <div
+        className="
+            flex 
+            flex-row 
+            ietms-center
+            justify-between
+            "
+      >
+        <div
+          className="
+            text-sm 
+            font-semibold
+            px-6
+            "
+        >
+          Anywhere
+        </div>
+
+        <div
+          className="
+            hidden
+            sm:block
+            text-sm 
+            font-semibold
+             px-6
+             border-x-[1px]
+            flex-1 
+            text-center
+            "
+        >
+          AnyDay
+        </div>
+
+        <div
+          className="
+            
+            text-sm 
+            font-semibold
+            pl-6
+            pr-2
+            tex-gray-600
+            flex
+            flex-row 
+            items-center
+            gap-3
+            -mt-1.5 
+            "
+        >
+          <div className="hidden sm:block"> Add Guests</div>
+          <div className="p-2 bg-rose-500 rounded-full text-white">
+            <BiSearch size={18} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
