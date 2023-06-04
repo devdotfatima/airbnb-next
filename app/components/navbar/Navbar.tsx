ba5f27dbb699da6389592b3c27e2753a0a2d80c8
Fatima Ali <fatimaaliforwork@gmail.com>
2023-06-04 16:54:17 +0500

    adds categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { SafeUser } from "@/app/types";
 
-// import Categories from "./Categories";
+import Categories from "./Categories";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
@@ -36,7 +36,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
-      {/* <Categories /> */}
+      <Categories />
     </div>
   );
 };
